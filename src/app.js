e1c2469abf507ab273df156f4c9f90db0e02e22a
charanvinay <charanvinaynarni@gmail.com>
2025-08-03 16:12:30 +0530

    files

--- src/app.js
+++ src/app.js
@@ -1,21 +1,37 @@
 import express from "express";
 import routes from "./routes/index.js";
 import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
+
 const app = express();
 
-const allowedOrigins = ['https://charanvinaynarni.netlify.app', 'http://localhost:2345'];
+// Handle __dirname in ES Modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      return callback(null, true);
-    } else {
-      return callback(new Error('Not allowed by CORS'));
-    }
-  }
-}));
-app.use(express.json())
-app.use("/uploads", express.static("uploads"))
+const allowedOrigins = [
+  "https://charanvinaynarni.netlify.app",
+  "http://localhost:2345",
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      } else {
+        return callback(new Error("Not allowed by CORS"));
+      }
+    },
+  })
+);
+
+app.use(express.json());
+
+// Serve uploads folder from absolute path
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
 app.use("/api", routes);
 
 export default app;
