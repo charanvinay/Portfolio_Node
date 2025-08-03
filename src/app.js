b2b3287179be320a3401453bfb8c9cb56628acb6
charanvinay-ael <charanvinay@apexcura.com>
2025-08-03 15:32:33 +0530

    vercel

--- src/app.js
+++ src/app.js
@@ -3,7 +3,17 @@ import routes from "./routes/index.js";
 import cors from "cors";
 const app = express();
 
-app.use(cors());
+const allowedOrigins = ['https://charanvinaynarni.netlify.app', 'http://localhost:2345'];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    } else {
+      return callback(new Error('Not allowed by CORS'));
+    }
+  }
+}));
 app.use(express.json())
 app.use("/uploads", express.static("uploads"))
 app.use("/api", routes);
